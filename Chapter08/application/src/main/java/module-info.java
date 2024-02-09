a427f22cfbd5ec4971e85461a98e6dc843b66151
cfernandezcairo <cfernandezcairo@gmail.com>
2024-02-08 21:55:38 -0500

    chapter08 commented module-info

--- Chapter08/application/src/main/java/module-info.java
+++ Chapter08/application/src/main/java/module-info.java
@@ -1,7 +1,10 @@
 module application {
+    //declaring module dependencies
+    requires domain;
+    requires static lombok;
+
+    //enable access to use cases and input/output ports
     exports dev.davivieira.topologyinventory.application.usecases;
     exports dev.davivieira.topologyinventory.application.ports.output;
     exports dev.davivieira.topologyinventory.application.ports.input;
-    requires domain;
-    requires static lombok;
 }
--- Chapter08/framework/src/main/java/module-info.java
+++ Chapter08/framework/src/main/java/module-info.java
@@ -1,13 +1,16 @@
 module framework {
+    //declaring module dependencies
     requires domain;
     requires application;
     requires static lombok;
+
     requires org.eclipse.persistence.core;
     requires java.sql;
     requires jakarta.persistence;
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.core;
 
+    //enable access to all public types
     exports dev.davivieira.topologyinventory.framework.adapters.output.h2.data;
-    opens dev.davivieira.topologyinventory.framework.adapters.output.h2.data;
+    opens dev.davivieira.topologyinventory.framework.adapters.output.h2.data;       //allow runtime reflective access to the output adapters
 }
